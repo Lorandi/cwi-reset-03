68552e653fd67ccfb97bc27ce805a1168fbb45d7
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-10-05 16:50:29 -0300

    Correct primeiroBug()

--- desafioAula1/registradora-cheia-de-bugs/src/Registradora.java
+++ desafioAula1/registradora-cheia-de-bugs/src/Registradora.java
@@ -2,9 +2,10 @@
 public class Registradora {
 
     public static void main(String[] args) {
-//        primeiroBug();
+        primeiroBug();
 
-        segundoBug();
+//        segundoBug();
+
 
 //        terceiroBug();
 //
