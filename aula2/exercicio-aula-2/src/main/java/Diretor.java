3e604cdacb5398a19cdca5bad6d306411a3ffbfb
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-10-09 10:20:08 -0300

    create Filme e Diretor

---
+++ aula2/exercicio-aula-2/src/main/java/Diretor.java
@@ -0,0 +1,35 @@
+public class Diretor {
+    String nome;
+    Integer idade;
+    Integer quantidadeFilmes;
+
+    public Diretor(String nome, Integer idade, Integer quantidadeFilmes) {
+        this.nome = nome;
+        this.idade = idade;
+        this.quantidadeFilmes = quantidadeFilmes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Integer getQuantidadeFilmes() {
+        return quantidadeFilmes;
+    }
+
+    public void setQuantidadeFilmes(Integer quantidadeFilmes) {
+        this.quantidadeFilmes = quantidadeFilmes;
+    }
+}
---
+++ aula2/exercicio-aula-2/src/main/java/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+    String nome;
+    String descricao;
+    String duracao;
+    Integer anoLancamento;
+    Integer avaliacao;
+    Diretor diretor;
+
+}
